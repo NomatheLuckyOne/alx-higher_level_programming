5d3a7905142754550d2d563e5ffcc0491f9de105
NomatheLuckyOne <thamiemathebula@gmail.com>
2022-09-12 08:00:45 -0700

    another+

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+        print(Ïnside result: {:.if}".format(result()
+    except:
+        result = None
+        print(Ïnside result: {}".format(result))
+    finally:
+        return result
