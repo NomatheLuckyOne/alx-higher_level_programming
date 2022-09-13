e68eb4bf3f5c436352e84ed67e7425a0ec754f4d
NomatheLuckyOne <thamiemathebula@gmail.com>
2022-09-12 22:52:28 -0700

    another+

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,9 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        print(Ïnside result: {:.if}".format(result()
+        print("Inside result: {:.if}".format(result))
     except:
         result = None
-        print(Ïnside result: {}".format(result))
+        print("Inside result: {}".format(result))
     finally:
         return result
