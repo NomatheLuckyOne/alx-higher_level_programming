7212b123a39326cbb7d3d00bcd573691580ad8c2
NomatheLuckyOne <thamiemathebula@gmail.com>
2022-09-21 20:53:10 -0700

    another+

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,4 +5,4 @@
 class LockedClass:
     """defines a class that prevents the user from dynamically creating
     attributes, except if it is first_name"""
-    _Noma_ = ["first_name"]
+    _slots_ = ["first_name"]
